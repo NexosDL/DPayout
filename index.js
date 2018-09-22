a27a6ef18c3fc54666b8e4af175c19412c25ed5a
NexosDL <43483660+NexosDL@users.noreply.github.com>
2018-09-22 11:28:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ bot.login(process.env.TOKEN)
 var prefix = ("?")
 
 bot.on('ready', function() {
-    bot.user.setPresence({ game: { name: '@Nexøs.#8825 | Prefix [?] ', type: 3} })
+    bot.user.setPresence({ game: { name: 'Nexøs. | Prefix [?] ', type: 2} })
     bot.user.setStatus('dnd');
     console.log("Connected");
 });
